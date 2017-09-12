0456c27e583fe303e416cb93b00fc400cdb3af30
Carlo Lam <calam12@gmail.com>
2017-09-11 22:24:07 -0700

    function and class view based django
    created a view based on a function and the same one as a class.

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
 # Create your views here.
+
+#function based view
+
+def kirr_redirect_view(request, *args, **kwargs):
+    return HttpResponse('hello')
+
+#class based view
+class KirrCBView(View):
+    def get(self, request, *args, **kwars):
+        return HttpResponse("Hello Again")
